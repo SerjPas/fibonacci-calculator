9a15bccdb7c8400568382e965e0abc3861eb48cc
SerjPas <passergiy@gmail.com>
2020-06-11 17:57:30 +0300

    Milestone 4 - done
    Run the following local server: ITC-fibonacci-server (read the readme!)
    Create a function that calls this server on this address: http://localhost:5050/fibonacci/:number, where :number is a parameter passed to the server to be calculated
    The response is the calculated fibonacci, present it to the user.
    Calling the server should replace your implementation of calculating fibonacci

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,15 @@
-
-function innerTextOnTheirPlace(){
+function callServer() {
     let xVariable = document.getElementById('number-input').value;
-    document.querySelector(".Y").innerText = calculateFibonacci(xVariable);
-}
-function calculateFibonacci(xVariable) {
-    let prev = 0, next = 1;
-    for (let i = 0; i < xVariable; i++) {
-        let temp = next;
-        next = prev + next;
-        prev = temp;
-    }
-    return prev;
-
-}
-function calculateFibonacciRecurcion(xVariable) {
-
-    if(n <= 1){
-        return xVariable;
-    }else{
-        return calculateFibonacciRecurcion(xVariable - 1) + calculateFibonacciRecurcion(xVariable - 2);
-    }
+    const SERVER_URL = `http://localhost:5050/fibonacci/${xVariable}`;
+    fetch(SERVER_URL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            const yVariable = document.querySelector(".Y");
+            yVariable.innerText = data.result;
+        });
 
 }
 
