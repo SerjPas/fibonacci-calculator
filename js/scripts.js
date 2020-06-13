fb49143fa16804aaa34d5b3df8bbd01052a42c26
SerjPas <passergiy@gmail.com>
2020-06-13 19:50:56 +0300

    Milestone 5
    Present a loader to the user when a call is made to the server (indicating the server is calculating) -done

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 function callServer() {
+
+
     let xVariable = document.getElementById('number-input').value;
     const SERVER_URL = `http://localhost:5050/fibonacci/${xVariable}`;
+    const y = document.querySelector('.Y');
+    const spinner = document.getElementById('spinner');
+    spinner.classList.remove('d-none');
+    y.classList.add('d-none');
     fetch(SERVER_URL)
         .then(function (response) {
             return response.json();
@@ -9,6 +15,8 @@ function callServer() {
             console.log(data);
             const yVariable = document.querySelector(".Y");
             yVariable.innerText = data.result;
+            spinner.classList.add('d-none');
+            y.classList.remove('d-none');
         });
 
 }
