3de7c6ab0fa104d95082d81b1fd0824bb0643335
SerjPas <passergiy@gmail.com>
2020-06-14 09:07:11 +0300

    Milestone 5. done
    Correct reactions of error messages added

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,31 @@
 function callServer() {
-
     let xVariable = document.getElementById('number-input').value;
+    let y = document.querySelector('.Y');
+
     const SERVER_URL = `http://localhost:5050/fibonacci/${xVariable}`;
-    const y = document.querySelector('.Y');
-    const spinner = document.getElementById('spinner');
+    let spinner = document.getElementById('spinner');
     spinner.classList.remove('d-none');
     y.classList.add('d-none');
-    const errorMoreThan50 = document.querySelector('.error50');
-    const input = document.querySelector('#number-input');
-    const error42 = document.querySelector('#error42')
+
+    let errorMoreThan50 = document.querySelector('.error50');
+    let input = document.querySelector('#number-input');
+    let error42 = document.querySelector('#error42')
 
     if (xVariable > 50) {
         errorMoreThan50.classList.remove("d-none");
-        input.classList.add('error50');
+        input.classList.add('error50-input');
 
     } else {
+        if(input.classList.contains('error50-input')){
+            input.classList.remove('error50-input')
+        }
+        if(!errorMoreThan50.classList.contains("d-none")){
+            errorMoreThan50.classList.add("d-none")
+        }
+        if(error42.classList.contains('is-present')){
+            error42.classList.add('d-none');
+        }
+
         fetch(SERVER_URL)
             .then(function (response) {
                 if (!response.ok) {
@@ -24,6 +35,7 @@ function callServer() {
             })
             .then(function (data) {
                 console.log(data);
+
                 const yVariable = document.querySelector(".Y");
                 yVariable.innerText = data.result;
                 spinner.classList.add('d-none');
@@ -31,8 +43,8 @@ function callServer() {
             })
             .catch(function (error) {
                 error42.innerText = error;
+                error42.setAttribute("class", "is-present")
                 spinner.classList.add('d-none');
-
             });
     }
 }
